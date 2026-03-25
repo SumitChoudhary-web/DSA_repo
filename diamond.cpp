41e81ffce3e36086be9e2de8acb5ea374e6c0e0a
SumitChoudhary-web <sc004811@gmail.com>
2026-03-25 23:27:07 +0530

    Diamondshape

---
+++ diamond.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter number of rows (half diamond): ";
+    cin >> n;
+
+    // Upper half of diamond
+    for(int i = 1; i <= n; i++) {
+
+        // Print spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for(int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Lower half of diamond
+    for(int i = n - 1; i >= 1; i--) {
+
+        // Print spaces
+        for(int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // Print stars
+        for(int j = 1; j <= (2 * i - 1); j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
